41014a58541f2daa1ea6fc5a4d4a38808c3ba1ad
kostyazaych <kostya.from.rostov@gmail.com>
2021-09-28 19:59:04 +0300

    AreaManager

---
+++ Assets/Mechanics/Scripts/AreaManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AreaManager : MonoBehaviour
+{
+    public Object StaticScene;
+    public Object LightScene;
+    public Object MechanicScene;
+    private Object[] scenesArray;
+    private int sceneIndex;
+    
+    
+    void Start()
+    {
+        scenesArray = new Object[] {StaticScene, LightScene,MechanicScene};
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        LoadAreaScene(scenesArray[sceneIndex]);
+    }
+
+    
+    
+    void LoadAreaScene(Object sceneObj)
+    {
+        if (!SceneManager.GetSceneByName(sceneObj.name).isLoaded)
+        {
+            SceneManager.LoadScene(sceneObj.name, LoadSceneMode.Additive);
+        }
+
+      else if (sceneIndex < scenesArray.Length - 1)
+        {
+            sceneIndex++;
+            LoadAreaScene(scenesArray[sceneIndex]);
+        }
+
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (sceneIndex < scenesArray.Length - 1)
+        {
+            sceneIndex++;
+            LoadAreaScene(scenesArray[sceneIndex]);
+        }
+    }
+}
